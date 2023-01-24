1a5ca28dd2e18d5c095f13f11f73f2c445b01c56
Mai Vang <vmaineng@gmail.com>
2023-01-24 15:38:11 -0800

    first commit

---
+++ server/routes/movie.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const router = express.Router()
+
+//get all movies
+router.get('/', (req, res) => {
+    res.json({msg: "get all movies"})
+})
+
+//get a single movie
+router.get('/:id', (req, res) => {
+    res.json({msg: "get a single movie"})
+})
+
+//post a new movie
+router.post('/', (req, res) => {
+    res.json({msg: "POST a new movie"})
+})
+
+//Delete a new movie
+router.delete('/:id', (req, res) => {
+    res.json({msg: "POST a new movie"})
+})
+
+
+//patch a new movie
+router.patch('/:id', (req, res) => {
+    res.json({msg: "POST a new movie"})
+})
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,32 @@
+//getting global obj from .env file
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const movieRoutes = require("./routes/movie");
+
+//creating express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//route handler
+app.use("/api/movies", movieRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
